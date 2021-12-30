d48b8734dd846a4ba04e9b6c78a612e500c3f7c6
qiyaozhu <54292642+qiyaozhu@users.noreply.github.com>
2021-12-29 19:49:03 -0500

    Genetic Algorithm

--- gaif.py
+++ gaif.py
@@ -576,7 +576,7 @@ def runGA(inpf, kwargs):
    nstill_1 = 100 # if the heavenList is not changed for up to 'nstill' iterations, mutate chromosomes in waiveList
    nstill_0 = 150 
 
-   nsurvivors = 10 # 500 # when the heaven has 'nsurvivors' chromosomes, stop the program 
+   nsurvivors = 500 # 500 # when the heaven has 'nsurvivors' chromosomes, stop the program 
 
    niter = 500 # number of iteration
 
